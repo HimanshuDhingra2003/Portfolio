6ec3c68b28d89cf3154ec8a2e846dc77a76541e9
Himanshu Dhingra <himdhi2003@gmail.com>
2023-09-29 21:30:21 +0530

    Update main.js

--- Portfolio1/main.js
+++ Portfolio1/main.js
@@ -1,5 +1,64 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", window.scrollY >120)
-})
+    header.classList.toggle("sticky", window.scrollY > 120);
+});
+
+const menuIcon = document.getElementById('menu-icon');
+const navList = document.querySelector('.navlist');
+
+// Media query for screens with a maximum width of 680px
+if (window.matchMedia('(max-width: 950px)').matches) {
+    menuIcon.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+}
+if (window.matchMedia('(max-width: 680px)').matches) {
+    menuIcon.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+}
+
+
+if (window.matchMedia('(max-width: 470px)').matches) {
+    menuIcon.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+}
+if (window.matchMedia('(max-width: 430px)').matches) {
+    menuIcon.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+    navList.classList.remove('active');
+
+
+    const navItems = document.querySelectorAll('.navlist a');
+    navItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            
+            navList.classList.remove('active');
+            menuIcon.classList.remove('active');
+        });
+    });
+});
+
+
+
+const checkbox = document.getElementById('check');
+        const navlist = document.querySelector('.navlist');
+
+        checkbox.addEventListener('change', () => {
+            if (checkbox.checked) {
+                navlist.style.right = '0';
+            } else {
+                navlist.style.right = '-100%';
+            }
+        });
