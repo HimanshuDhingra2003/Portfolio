b162beb03c104458af194a25365aaa470f8665f9
Himanshu Dhingra <himdhi2003@gmail.com>
2023-10-01 03:13:52 +0530

    Update main.js

--- Portfolio1/main.js
+++ Portfolio1/main.js
@@ -62,3 +62,75 @@ const checkbox = document.getElementById('check');
                 navlist.style.right = '-100%';
             }
         });
+
+// Function to handle smooth scrolling
+function smoothScroll(target) {
+  const targetElement = document.querySelector(target);
+  if (targetElement) {
+    window.scrollTo({
+      top: targetElement.offsetTop - header.offsetHeight,
+      behavior: 'smooth',
+    });
+  }
+}
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+  header.classList.toggle('sticky', window.scrollY > 120);
+});
+
+const menuIcon = document.getElementById('menu-icon');
+const navList = document.querySelector('.navlist');
+
+// Media query for screens with a maximum width of 950px
+if (window.matchMedia('(max-width: 950px)').matches) {
+  menuIcon.addEventListener('click', () => {
+    navList.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+  });
+}
+
+// Code for handling smooth scrolling when links are clicked
+const navItems = document.querySelectorAll('.navlist a');
+
+navItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Get the target from the href attribute
+    const target = item.getAttribute('href');
+
+    // Close the mobile menu (if open)
+    navList.classList.remove('active');
+    menuIcon.classList.remove('active');
+
+    // Smooth scroll to the target section
+    smoothScroll(target);
+  });
+});
+
+// Handle checkbox change (for mobile menu)
+const checkbox = document.getElementById('check');
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    navlist.style.right = '0';
+  } else {
+    navlist.style.right = '-100%';
+  }
+});
+
+// Function to scroll to the top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+// Optional: Scroll to top button functionality
+const scrollToTopButton = document.getElementById('scroll-to-top-button');
+if (scrollToTopButton) {
+  scrollToTopButton.addEventListener('click', scrollToTop);
+}
+
